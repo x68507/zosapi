bd0f4fd87b35527984814732b165d64fcf19e936
Michael Humphreys <michael.humphreys@zemax.com>
2019-07-11 16:39:44 -0700

    Initial Commit

---
+++ zosapi.py
@@ -0,0 +1,114 @@
+import clr, os, winreg, ctypes, sys
+import numpy as np
+from System.Runtime.InteropServices import GCHandle, GCHandleType
+
+
+class PythonStandaloneApplication(object):
+    class LicenseException(Exception):
+        pass
+    class ConnectionException(Exception):
+        pass
+    class InitializationException(Exception):
+        pass
+    class SystemNotPresentException(Exception):
+        pass
+
+    def __init__(self, path=None):
+        # determine location of ZOSAPI_NetHelper.dll & add as reference
+        aKey = winreg.OpenKey(winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER), r"Software\Zemax", 0, winreg.KEY_READ)
+        zemaxData = winreg.QueryValueEx(aKey, 'ZemaxRoot')
+        NetHelper = os.path.join(os.sep, zemaxData[0], r'ZOS-API\Libraries\ZOSAPI_NetHelper.dll')
+        winreg.CloseKey(aKey)
+        clr.AddReference(NetHelper)
+        import ZOSAPI_NetHelper
+        
+        path = r'C:\Program Files\Zemax OpticStudio 181120\\'
+        
+        # Find the installed version of OpticStudio
+        #if len(path) == 0:
+        if path is None:
+            isInitialized = ZOSAPI_NetHelper.ZOSAPI_Initializer.Initialize()
+        else:
+            # Note -- uncomment the following line to use a custom initialization path
+            isInitialized = ZOSAPI_NetHelper.ZOSAPI_Initializer.Initialize(path)
+        
+        # determine the ZOS root directory
+        if isInitialized:
+            dir = ZOSAPI_NetHelper.ZOSAPI_Initializer.GetZemaxDirectory()
+        else:
+            raise PythonStandaloneApplication.InitializationException("Unable to locate Zemax OpticStudio.  Try using a hard-coded path.")
+
+        # add ZOS-API referencecs
+        clr.AddReference(os.path.join(os.sep, dir, "ZOSAPI.dll"))
+        clr.AddReference(os.path.join(os.sep, dir, "ZOSAPI_Interfaces.dll"))
+        import ZOSAPI
+
+        # create a reference to the API namespace
+        self.ZOSAPI = ZOSAPI
+
+        # create a reference to the API namespace
+        self.ZOSAPI = ZOSAPI
+
+        # Create the initial connection class
+        self.TheConnection = ZOSAPI.ZOSAPI_Connection()
+
+        if self.TheConnection is None:
+            raise PythonStandaloneApplication.ConnectionException("Unable to initialize .NET connection to ZOSAPI")
+
+        self.TheApplication = self.TheConnection.CreateNewApplication()
+        if self.TheApplication is None:
+            raise PythonStandaloneApplication.InitializationException("Unable to acquire ZOSAPI application")
+
+        if self.TheApplication.IsValidLicenseForAPI == False:
+            raise PythonStandaloneApplication.LicenseException("License is not valid for ZOSAPI use")
+
+        self.TheSystem = self.TheApplication.PrimarySystem
+        if self.TheSystem is None:
+            raise PythonStandaloneApplication.SystemNotPresentException("Unable to acquire Primary system")
+
+    def __del__(self):
+        if self.TheApplication is not None:
+            self.TheApplication.CloseApplication()
+            self.TheApplication = None
+        
+        self.TheConnection = None
+    
+    def OpenFile(self, filepath, saveIfNeeded):
+        if self.TheSystem is None:
+            raise PythonStandaloneApplication10.SystemNotPresentException("Unable to acquire Primary system")
+        self.TheSystem.LoadFile(filepath, saveIfNeeded)
+
+    def CloseFile(self, save):
+        if self.TheSystem is None:
+            raise PythonStandaloneApplication10.SystemNotPresentException("Unable to acquire Primary system")
+        self.TheSystem.Close(save)
+
+    def SamplesDir(self):
+        if self.TheApplication is None:
+            raise PythonStandaloneApplication10.InitializationException("Unable to acquire ZOSAPI application")
+
+        return self.TheApplication.SamplesDir
+
+    def ExampleConstants(self):
+        if self.TheApplication.LicenseStatus == self.ZOSAPI.LicenseStatusType.PremiumEdition:
+            return "Premium"
+        elif self.TheApplication.LicenseStatus == self.ZOSAPI.LicenseStatusTypeProfessionalEdition:
+            return "Professional"
+        elif self.TheApplication.LicenseStatus == self.ZOSAPI.LicenseStatusTypeStandardEdition:
+            return "Standard"
+        else:
+            return "Invalid"
+    
+    def DoubleToNumpy(self, data):
+        if 'numpy' not in sys.modules:
+            print('You have not imported numpy into this file')
+            return False
+        else:
+            src_hndl = GCHandle.Alloc(data, GCHandleType.Pinned)
+            try:
+                src_ptr = src_hndl.AddrOfPinnedObject().ToInt64()
+                cbuf = (ctypes.c_double*len(data)).from_address(src_ptr)
+                npData = np.frombuffer(cbuf, dtype=np.float64)
+            finally:
+                if src_hndl.IsAllocated: src_hndl.Free()
+            return npData
