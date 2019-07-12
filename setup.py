22137d3310db46f7ea9d3d432aa8f6b987f3cc0d
Michael Humphreys <michael.humphreys@zemax.com>
2019-07-11 17:07:43 -0700

    Files to register 'zosapi' with PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'zosapi',
+  packages = ['zosapi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A class to connect to Zemax OpticStudio API via .NET',
+  author = 'Michael Humphreys',
+  author_email = 'michael.humphreys@zemax.com',
+  url = 'https://github.com/x68507/zosapi',
+  download_url = 'https://github.com/x68507/zosapi/tarball/0.1',
+  keywords = ['zosapi', 'zemax', 'opticstudio'], 
+  classifiers = [],
+)
